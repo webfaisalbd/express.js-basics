cc9958b05289927460b7a8d484549dbdc7ebdc5f
webfaisalbd <web.faisal.bd@gmail.com>
2022-07-27 10:27:28 +0600

    http response

--- Lesson8/app.js
+++ Lesson8/app.js
@@ -5,8 +5,24 @@ const userRouter = require('./routes/user.route')
 app.use('/api/user',userRouter);
 
 app.get('/',(req,res)=>{
-    res.send("<h2>This is home route.");
+    res.statusCode = 200;
+    res.sendFile(__dirname+ '/views/index.html')
 })
+
+app.get("/register",(req,res)=>{
+    res.statusCode = 200;
+    res.sendFile(__dirname+ '/views/register.html')
+})
+app.get("/login",(req,res)=>{
+    res.statusCode = 200;
+    res.send("I am login, be alert")
+    // res.cookie("name","tasnim");
+    res.cookie("age","30");
+    res.clearCookie("name");
+    res.append("id","16141");
+    res.end();
+})
+
 
 app.use((req,res)=> {
     res.send("<h2>404!!! Not a valid URL</h2>");
